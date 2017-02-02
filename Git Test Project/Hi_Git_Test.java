187771aabab85ec1e742b79305b68763cd506f32
Douglas Linkhart <dlinkhart@hawkmail.hccfl.edu>
2017-02-02 00:51:33 -0500

    With message box!

--- Git Test Project/Hi_Git_Test.java
+++ Git Test Project/Hi_Git_Test.java
@@ -1,9 +1,13 @@
 
+import javax.swing.JOptionPane;
+
 public class Hi_Git_Test {
 
 	public static void main ( String [] args ) {
 	
 		System.out.println("test 2!");
+		
+		JOptionPane.showMessageDialog(null,"Best wishes for another successful Java course from Douglas Linkhart!","Greetings",JOptionPane.INFORMATION_MESSAGE);
 		
 		// Change made here.
 	}
